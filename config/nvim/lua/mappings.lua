c726e9f7c490ddab120ee34e301dbb8a1d00edce
David Sanson <dsanson@gmail.com>
2024-07-28 23:13:03 -0500

    updating mappings to not be so reliant on which-key

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -1,39 +1,46 @@
-vim.g.mapleader      = ' '
-vim.g.maplocalleader = ','
+local cmd = vim.cmd
+local g = vim.g
+local keymap = vim.keymap
+local api = vim.api
+local lsp = vim.lsp
+
+g.mapleader      = ' '
+g.maplocalleader = ','
 
 -- Aliases
-vim.cmd('command! W w') -- :W is the same as :w
-vim.cmd('command! Wq wq') -- :W is the same as :w
+cmd('command! W w') -- :W is the same as :w
+cmd('command! Wq wq') -- :W is the same as :w
 
 
 -- Treat long lines as break lines (useful when moving around in them)
-vim.api.nvim_set_keymap('n', 'j', 'gj', {noremap = true} )
-vim.api.nvim_set_keymap('n', 'k', 'gk', {noremap = true} )
-vim.api.nvim_set_keymap('v', 'j', 'gj', {noremap = true} )
-vim.api.nvim_set_keymap('v', 'k', 'gk', {noremap = true} )
+keymap.set('n', 'j', 'gj')
+keymap.set('n', 'k', 'gk')
+keymap.set('v', 'j', 'gj')
+keymap.set('v', 'k', 'gk')
 
 -- Up and Down arrows by screen lines rather than file lines)
-vim.api.nvim_set_keymap('n', '<Down>', 'gj', {noremap = true} )
-vim.api.nvim_set_keymap('n', '<Up>',   'gk', {noremap = true} )
+keymap.set('n', '<Down>', 'gj')
+keymap.set('n', '<Up>',   'gk')
 
 -- visual shifting without exiting visual mode
-vim.api.nvim_set_keymap('v', '<', '<gv', { desc = "shift left" })
-vim.api.nvim_set_keymap('v', '>', '>gv', { desc = "shift right" })
+keymap.set('v', '<', '<gv', { desc = "shift left" })
+keymap.set('v', '>', '>gv', { desc = "shift right" })
 
 -- Remap Y to yank to end of line (so consistent with C and D)
-vim.api.nvim_set_keymap('n', 'Y', 'y$', {noremap = true} )
-vim.api.nvim_set_keymap('v', '<Enter>', ':EasyAlign<CR>', {noremap = true, desc = "Easy align"} ) 
+keymap.set('n', 'Y', 'y$' )
+
+-- Easy Align mapping
+keymap.set('v', '<Enter>', ':EasyAlign<CR>', {desc = "Easy align"} )
 
 -- Open main notes file
-vim.keymap.set('n', 'gn', '<cmd>edit ~/d/zettel/Home.md<cr>', { desc = "open notes" } )
-
+keymap.set('n', 'gn', '<cmd>edit ~/d/zettel/Home.md<cr>', { desc = "open notes" } )
 
 -- LSP mappings 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+api.nvim_create_autocmd('LspAttach', {
+  group = api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(args)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf, desc = "goto definition" })
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = args.buf, desc = "goto references" })
-    vim.keymap.set('n', 'cd', vim.lsp.buf.code_action, { buffer = args.buf, desc = "code actions" })
+    keymap.set('n', 'gd', lsp.buf.definition, { buffer = args.buf, desc = "goto definition" })
+    keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = args.buf, desc = "goto references" })
+    keymap.set('n', 'cd', lsp.buf.code_action, { buffer = args.buf, desc = "code actions" })
   end
 })
--- config/nvim/lua/mappings_add.lua
+++ config/nvim/lua/mappings_add.lua
@@ -1,252 +1,92 @@
--- Which-key maps
+local cmd = vim.cmd
+local g = vim.g
+local o = vim.o
+local keymap = vim.keymap
+local api = vim.api
+local lsp = vim.lsp
 local wk = require('which-key')
 
-wk.register({
-  -- File mappings
-  f = {
-    name = "file",
-    f = { "<cmd>Telescope find_files hidden=true no_ignore=true<cr>", "Find File" }, -- create a binding with label
-    -- f = { require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({})), "Find File" }, -- create a binding with label
-    g = { "<cmd>Telescope live_grep<cr>", "Grep Files" }, -- create a binding with label
-    r = { "<cmd>Telescope oldfiles<cr>", "Recent Files"},
-    b = { "<cmd>Telescope buffers<cr>", "Switch buffer"},
-    z = { "<cmd>Telescope zoxide list<cr>", "Zoxide"},
-  },
+keymap.set('n', '<leader><leader>', '<cmd>Telescope commands<cr>', { desc = "Run command" })
 
-  -- Quitting
-  q = {
-    name = "quitting",
-    q = { "<cmd>qw<cr>", "Save and quit" }, -- create a binding with label
-    x = { "<cmd>q!<cr>", "Quit without saving" }, -- create a binding with label
-  },
+wk.add({ { "<leader>f", group = "file" } })
+keymap.set('n', '<leader>ff', '<cmd>Telescope find_files hidden=true no_ignore=true<cr>', { desc = "Find file" })
+keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = "Grep files" })
+keymap.set('n', '<leader>fr', '<cmd>Telescope oldfiles<cr>', { desc = "Recent files" })
+keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = "Switch buffer" })
+keymap.set('n', '<leader>fz', '<cmd>Telescope zoxide list<cr>', { desc = "Zoxide" })
 
-  -- Buffer mappings
-  ['<space>'] =  { '<cmd>Telescope commands<cr>', "Run command" },
-  b = {
-    name = "buffer",
-    b = { "<cmd>Telescope buffers theme=dropdown<cr>", "Switch buffer"},
-    d = { "<cmd>bdelete<cr>", "Kill buffer" }, -- create a binding with label
-    n = { "<cmd>bnext<cr>", "Next buffer" }, -- create a binding with label
-    p = { "<cmd>bprevious<cr>", "Previous buffer" }, -- create a binding with label
-  },
+wk.add({ { "<leader>q", group = "quitting" } })
+keymap.set('n', '<leader>qq', '<cmd>qw<cr>', { desc = "Save and quit" })
+keymap.set('n', '<leader>qx', '<cmd>q!<cr>', { desc = "Quit without saving" })
 
-  -- Diagnostics
-  d = {
-    name = "diagnostics",
-    p  = { vim.diagnostic.goto_prev, 'go to previous' },
-    n  = { vim.diagnostic.goto_next, 'go to next' },
-  },
+wk.add({ { "<leader>b", group = "buffer" } })
+keymap.set('n', '<leader>bb', '<cmd>Telescope buffers theme=dropdown<cr>', { desc = "Switch buffer" })
+keymap.set('n', '<leader>bd', '<cmd>bdelete<cr>', { desc = "Switch buffer" })
+keymap.set('n', '<leader>bn', '<cmd>bnext<cr>', { desc = "Next buffer" })
+keymap.set('n', '<leader>bp', '<cmd>bprevious<cr>', { desc = "Previous buffer" })
+keymap.set('n', '<leader>bt', '<cmd>terminal<cr>', { desc = "Open terminal buffer" })
 
-  -- Jump mappings
-  j = {
-    name = "jump",
-    z = { "<cmd>!open \"$(bib2path2 -z \"<cWORD>\")\"<CR>", 'open in Zotero'},
-    t = { "<cmd>!kitty --single-instance --instance-group=1 /Users/desanso/bin/termpdf.py --nvim-listen-address $NVIM_LISTEN_ADDRESS --open <cword><CR><CR>", "Open PDF in termpdf.py"},
-    p = { "<cmd>!open \"$(bib2path2 \"<cWORD>\")\"<CR>", "Open PDF in Preview.app"},
-    x = { 'gx', 'Open file (gx)' },
-    f = { 'gF', 'Open file in nvim (gF)' },
-    h = { '<cmd>Telescope heading<cr>', 'Jump to heading' },
-    b = { '<cmd>Telescope bookmarks<cr>', 'Open browser bookmark'},
-    o = { '<cmd>!open \"obsidian://open?vault=Everything&file=%:t:r\"<cr>', 'Open in Obsidian'},
-    d = { '<cmd>Telescope lsp_definitions<cr>', 'Jump to lsp definition'},
-    r = { '<cmd>Telescope lsp_references<cr>', 'Jump to lsp reference'},
-  },
+wk.add({ { "<leader>d", group = "diagnostics" } })
+keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { desc = "Go to previous" })
+keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { desc = "Go to next" })
 
-  -- marks
-  m = {
-    name = "marks",
-    a = {'<cmd>BookmarkAnnotate<cr>',"add/edit/remove annotation"},
-    t = {'<cmd>BookmarkToggle<cr>','add/remove bookmark'},
-    n = {'<cmd>BookmarkNext<cr>','jump to next bookmark'},
-    p = {'<cmd>BookmarkPrev<cr>','jump to previous bookmark'},
-    s = {'<cmd>Telescope vim_bookmarks current_file<cr>', 'search bookmarks in current buffer'},
-    S = {'<cmd>Telescope vim_bookmarks all<cr>', 'search all bookmarks'},
-    m = {'<cmd>Telescope marks<cr>', 'search all marks'},
-  },
+wk.add({ { "<leader>j", group = "jump" } })
+keymap.set('n', '<leader>jz', "<cmd>!open \"$(bib2path2 -z \"<cWORD>\")\"<CR>", { desc = "Open in Zotero" })
+keymap.set('n', '<leader>jp', "<cmd>!open \"$(bib2path2 \"<cWORD>\")\"<CR>", { desc = "Open PDF in Preview.app"})
+keymap.set('n', '<leader>jx', 'gx', { desc = 'Open file (gx)' })
+keymap.set('n', '<leader>jf', 'gF', { desc = 'Open file in nvim (gF)' })
+keymap.set('n', '<leader>jh', '<cmd>Telescope heading<cr>', { desc = 'Jump to heading' })
+keymap.set('n', '<leader>jb', '<cmd>Telescope bookmarks<cr>', { desc = 'Jump to bookmark' })
+keymap.set('n', '<leader>jo', '<cmd>!open \"obsidian://open?vault=Everything&file=%:t:r\"<cr>', { desc = 'Open in Obsidian' })
+keymap.set('n', '<leader>jd',  '<cmd>Telescope lsp_definitions<cr>', { desc = 'Jump to lsp definition' })
+keymap.set('n', '<leader>jr',  '<cmd>Telescope lsp_references<cr>', { desc = 'Jump to lsp reference' })
 
-  w = {
-    name = "writing",
-    b = { "<cmd>.!bib2path2 -b \"<cWORD>\"<CR>",
-          "Replace citekey with bibliographic entry"},
-  }, 
+wk.add({ { "<leader>m", group = "marks" } })
+keymap.set('n', '<leader>ma', '<cmd>BookmarkAnnotate<cr>', { desc = "add/edit/remove annotation"})
+keymap.set('n', '<leader>mt', '<cmd>BookmarkToggle<cr>', { desc = "add/remove bookmark"})
+keymap.set('n', '<leader>mn', '<cmd>BookmarkNext<cr>', { desc = "Next bookmark"})
+keymap.set('n', '<leader>mp', '<cmd>BookmarkPrev<cr>', { desc = "Previous bookmark"})
+keymap.set('n', '<leader>ms', '<cmd>Telescope vim_bookmarks current_file<cr>', { desc = "Search bookmarks in buffer"})
+keymap.set('n', '<leader>mS', '<cmd>Telescope vim_bookmarks all<cr>', { desc = "Search all bookmarks"})
+keymap.set('n', '<leader>mm', '<cmd>Telescope marks<cr>', { desc = "Search all marks"})
 
-  -- toggles
-  t = {
-    name = "toggle",
-    w = { "<cmd>set nolist!<cr>", 'Show invisibles' },
-    a = { "<cmd>set arabic!<cr>", "Arabic mode" },
-    z = { "<cmd>ZenMode<cr>", "Zen mode" },
-    d = { "<cmd>Twilight<cr>", "Twilight mode" },
-    t = { "<cmd>Neotree document_symbols toggle<cr>", "Show table of contents"},
-    f = { "<cmd>Neotree filesystem toggle<cr>", "Show filesystem"},
-    b = { "<cmd>Neotree buffers toggle<cr>", "Show buffers"},
-    -- c = { "<cmd>source ~/.vimrc_background<cr>", "Apply system colorscheme"},
-    c = { function () vim.o.cursorline = not vim.o.cursorline end, "Toggle cursorline" },
-  },
+wk.add({ { "<leader>w", group = "writing" } })
+keymap.set('n', '<leader>wb', "<cmd>.!bib2path2 -b \"<cWORD>\"<CR>", { desc = "replace citekey with bibliographic entry" })
 
-  -- inserts
-  i = {
-    name = "insert",
-    d = {'"=strftime("%Y-%m-%d")<CR>p', "Insert today's date" },
-    t = {'"=strftime("%FT%T%Z")<CR>p', "Insert timestamp"},  
-    e = {function() require'telescope.builtin'.symbols{ sources = {'emoji'} } end, "Insert emoji"},
-    m = {function() require'telescope.builtin'.symbols{ sources = {'math'} } end, "Insert math symbol"},
-    l = {function() require'telescope.builtin'.symbols{ sources = {'logic'} } end, "Insert logic symbol"},
-    b = { "<cmd>Telescope bibtex<cr>", "Insert bib key"},
-  },
+wk.add({ { "<leader>t", group = "toggles"} })
+keymap.set('n', '<leader>tw', "<cmd>set nolist!<cr>", { desc = "Show invisibles" })
+keymap.set('n', '<leader>ta', "<cmd>set arabic!<cr>", { desc = "Arabic mode" })
+keymap.set('n', '<leader>tz', "<cmd>ZenMode<cr>", { desc = "Zen mode" })
+keymap.set('n', '<leader>td', "<cmd>Twilight<cr>", { desc = "Twilight mode" })
+keymap.set('n', '<leader>tt', "<cmd>Neotree document_symbols toggle<cr>", { desc = "Table of Contents" })
+keymap.set('n', '<leader>tf', "<cmd>Neotree filesystem toggle<cr>", { desc = "Filesystem" })
+keymap.set('n', '<leader>tb', "<cmd>Neotree buffers toggle<cr>", { desc = "Buffers" })
+keymap.set('n', '<leader>tc', function () o.cursorline = not o.cursorline end, { desc = "Toggle cursorline" })
 
-  -- carnap
-  C = {
-    name = "carnap",
-    p = {'<cmd>!carnap put %<cr>', "Upload to Carnap"},
-    o = {'<cmd>!carnap open %<cr>', "Open on Carnap"},
-  },
+wk.add({ { "<leader>i", group = "inserts"} })
+keymap.set('n', '<leader>id', '"=strftime("%Y-%m-%d")<CR>p', { desc = "Insert today's date" })
+keymap.set('n', '<leader>it', '"=strftime("%FT%T%Z")<CR>p', { desc = "Insert timestamp" })
+keymap.set('n', '<leader>ie', function() require'telescope.builtin'.symbols{ sources = {'emoji'} } end, { desc = "Insert emoji" })
+keymap.set('n', '<leader>im', function() require'telescope.builtin'.symbols{ sources = {'math'} } end, { desc = "Insert math symbol" })
+keymap.set('n', '<leader>il', function() require'telescope.builtin'.symbols{ sources = {'logic'} } end, { desc = "Insert logic symbol" })
+keymap.set('n', '<leader>ib',  "<cmd>Telescope bibtex<cr>", { desc = "Insert bib key" })
 
-  -- -- zk
-  -- z = {
-  --   name = "notes",
-  --   n = {"<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>","New note"},
-  --   i = {"<Cmd>ZkIndex<CR>","New note"},
-  --   o = {"<Cmd>ZkNotes { sort = { 'modified' } }<CR>","Open note"},
-  --   t = {"<Cmd>ZkTags<CR>","Search by tags"},
-  --   b = {"<Cmd>ZkBacklinks { sort = { 'modified' } }<CR>","Open backlinks"},
-  --   l = {"<Cmd>ZkLinks { sort = { 'modified' } }<CR>","Open links"},
-  -- },
- 
-  -- spelling
-  s = {
-    name = 'spelling',
-    ['='] = {'<cmd>Telescope spell_suggest<cr>', 'Suggest correct spelling'},
-    n = {']s', 'Go to next mispelled word'},
-    a = {'zg', 'Add word to dictionary'},
-    x = {'zw', 'Mark as mispelled'},
-  },
+wk.add({ { "<leader>C", group = "carnap"} })
+keymap.set('n', '<leader>Cp', '<cmd>!carnap put %<cr>', { desc = "Upload to Carnap" })
+keymap.set('n', '<leader>Co', '<cmd>!carnap open %<cr>', { desc = "Open on Carnap" })
 
-  -- clipboard
-  c = {
-    name = 'clipboard',
-    s = {'<cmd>Telescope neoclip<cr>', 'Search old clippings'},
-  },
+wk.add({ { "<leader>c", group = "clipboard"} })
+keymap.set('n', '<leader>cs', '<cmd>Telescope neoclip<cr>', { desc = "Search old clippings" })
 
-  -- help
-  h = {
-    name = "help",
-    h = {'<cmd>Telescope help_tags<cr>', 'Get help'},
-  },
-
-}, 
-{ 
-  prefix = "<leader>",
-  mode = 'n',
-  disable = {
-    buftypes = {},
-    filetypes = { "TelescopePrompt" },
-  },
-})
+wk.add({ { "<leader>h", group = "help"} })
+keymap.set('n', '<leader>hh', '<cmd>Telescope help_tags<cr>', { desc = "Search help" })
+keymap.set('n', '<leader>hm', '<cmd>Telescope man_pages<cr>', { desc = "Search man pages" })
 
 -- Terminal Mode mappings
 
 local function termcodes(str)
-   return vim.api.nvim_replace_termcodes(str, true, true, true)
+   return api.nvim_replace_termcodes(str, true, true, true)
 end
 
--- remap so can use ESC in terminal
-wk.register({
-  ['<Esc>'] = {termcodes '<C-\\><C-n>', 'normal mode'},
-},
-{
-    prefix = "",
-    mode = 't'
-})
+keymap.set('t', '<leader><Esc>', termcodes '<C-\\><C-n>')
 
--- but unremap if it is actually just fzf
-vim.api.nvim_command('au! FileType fzf silent! tunmap <Esc>')
-
-wk.register({
-  -- File mappings
-  f = {
-    name = "file",
-    f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-    -- f = { require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({})), "Find File" }, -- create a binding with label
-    g = { "<cmd>Telescope live_grep<cr>", "Grep Files" }, -- create a binding with label
-    r = { "<cmd>Telescope oldfiles<cr>", "Recent Files"},
-    b = { "<cmd>Telescope buffers<cr>", "Switch buffer"},
-    z = { "<cmd>Telescope zoxide list<cr>", "Zoxide"},
-  },
-
-  -- Quitting
-  q = {
-    name = "quitting",
-    q = { '<cmd>wq<cr>' , "Save and quit" }, -- create a binding with label
-    x = { "<cmd>q!<cr>", "Quit without saving" }, -- create a binding with label
-  },
-
-  -- Buffer mappings
-  -- ['<space>'] =  { '<cmd>Telescope commands<cr>', "Run command" },
-  b = {
-    name = "buffer",
-    b = { "<cmd>Telescope buffers theme=dropdown<cr>", "Switch buffer"},
-    d = { "<cmd>bdelete<cr>", "Kill buffer" }, -- create a binding with label
-    n = { "<cmd>bnext<cr>", "Next buffer" }, -- create a binding with label
-    p = { "<cmd>bprevious<cr>", "Previous buffer" }, -- create a binding with label
-  },
-
-  -- Jump mappings
-  j = {
-    name = "jump",
-    z = { "<cmd>!open \"$(bib2path2 -z \"<cWORD>\")\"<CR>", 'open in Zotero'},
-    t = { "<cmd>!kitty --single-instance --instance-group=1 /Users/desanso/bin/termpdf.py --nvim-listen-address $NVIM_LISTEN_ADDRESS --open <cword><CR><CR>", "Open PDF in termpdf.py"},
-    p = { "<cmd>!open \"$(bib2path2 \"<cWORD>\")\"<CR>", "Open PDF in Preview.app"},
-    x = { 'gx', 'Open file (gx)' },
-    f = { 'gf', 'Open file in nvim (gf)' },
-    b = { '<cmd>Telescope bookmarks<cr>', 'Open browser bookmark'},
-  },
-
-  -- marks
-  m = {
-    name = "marks",
-    s = {'<cmd>Telescope vim_bookmarks all<cr>', 'search all bookmarks'},
-    m = {'<cmd>Telescope marks<cr>', 'search all marks'},
-  },
-
-  -- toggles
-  t = {
-    name = "toggle",
-    -- c = { "<cmd>source ~/.vimrc_background<cr>", "Apply system colorscheme"},
-    c = { function () vim.o.cursorline = not vim.o.cursorline end, "Toggle cursorline" },
-  },
-
-  -- inserts
-  i = {
-    name = "insert",
-    d = {'"=strftime("%Y-%m-%d")<CR>p', "Insert today's date" },
-    t = {'"=strftime("%FT%T%Z")<CR>p', "Insert timestamp"},  
-    e = {function() require'telescope.builtin'.symbols{ sources = {'emoji'} } end, "Insert emoji"},
-    m = {function() require'telescope.builtin'.symbols{ sources = {'math'} } end, "Insert math symbol"},
-    l = {function() require'telescope.builtin'.symbols{ sources = {'logic'} } end, "Insert logic symbol"},
-  },
-
-  -- -- zk
-  -- z = {
-  --   name = "notes",
-  --   n = {"<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>","New note"},
-  --   o = {"<Cmd>ZkNotes { sort = { 'modified' } }<CR>","Open note"},
-  --   t = {"<Cmd>ZkTags<CR>","Search by tags"},
-  -- },
-
-  -- help
-  h = {
-    name = "help",
-    h = {'<cmd>Telescope help_tags<cr>', 'Get help'},
-    m = {'<cmd>Telescope man_pages<cr>', 'Man pages'},
-  },
-
-}, 
-{ 
-  prefix = "<leader>",
-  mode = 't',
-  disable = {
-    buftypes = {},
-    filetypes = { "TelescopePrompt" },
-  },
-})
