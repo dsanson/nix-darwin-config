27feb2cab8cc5ce132ea3402852e8b2a185f3d98
David Sanson <dsanson@gmail.com>
2025-04-29 21:13:33 -0500

    commenting out unused fish plugins

--- home.nix
+++ home.nix
@@ -551,17 +551,8 @@ in
         end
       '';
       plugins = [
-        { name = "bass"; src = pkgs.fishPlugins.bass; }
-        { name = "foreign-env"; src = pkgs.fishPlugins.foreign-env; }
-        {
-          name = "pure";
-          src = pkgs.fetchFromGitHub {
-            owner = "pure-fish";
-            repo = "pure";
-            rev = "28447d2e7a4edf3c954003eda929cde31d3621d2";
-            hash = "sha256-8zxqPU9N5XGbKc0b3bZYkQ3yH64qcbakMsHIpHZSne4=";
-          };
-        }
+        # { name = "bass"; src = pkgs.fishPlugins.bass; }
+        # { name = "foreign-env"; src = pkgs.fishPlugins.foreign-env; }
         {
           name = "fish-completion-pandoc";
           src = pkgs.fetchFromGitHub {
@@ -580,15 +571,15 @@ in
             hash = "sha256-T8KYLA/r/gOKvAivKRoeqIwE2pINlxFQtZJHpOy9GMM=";
           };
         }
-        {
-          name = "fishopts";
-          src = pkgs.fetchFromGitHub {
-            owner = "jorgebucaran";
-            repo = "fishopts";
-            rev = "4b74206725c3e11d739675dc2bb84c77d893e901";
-            hash = "sha256-9hRFBmjrCgIUNHuOJZvOufyLsfreJfkeS6XDcCPesvw=";
-          };
-        }
+        # {
+        #   name = "fishopts";
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "jorgebucaran";
+        #     repo = "fishopts";
+        #     rev = "4b74206725c3e11d739675dc2bb84c77d893e901";
+        #     hash = "sha256-9hRFBmjrCgIUNHuOJZvOufyLsfreJfkeS6XDcCPesvw=";
+        #   };
+        # }
       ];
     };
 
