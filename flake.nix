9b419ab5576d00d1a00c52ef6ffab7cd962363cb
David Sanson <dsanson@gmail.com>
2025-05-11 11:26:04 -0500

    updating lix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-darwin = {
