689e975ce0c232d3be90728dd049f99111d7860e
David Sanson <dsanson@gmail.com>
2026-06-24 21:58:46 -0500

    updating sketchybar for new laptop

--- config/sketchybar/plugins/battery.sh
+++ config/sketchybar/plugins/battery.sh
@@ -1,22 +1,28 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-PERCENTAGE=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
+PERCENTAGE=$(pmset -g batt | grep -Eo "[0-9]+%" | cut -d% -f1)
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 # no icon if charging
 if [ "$CHARGING" != "" ]; then
   ICON="⚡︎"
 else
-  case ${PERCENTAGE} in
-    9[0-9]|100) ICON=""
+  case "$PERCENTAGE" in
+    9[0-9]|100)
+      ICON=""
     ;;
-    [6-8][0-9]) ICON=""
+    [6-8][0-9]) 
+      ICON=""
     ;;
-    [3-5][0-9]) ICON=""
+    [3-5][0-9]) 
+      ICON=""
     ;;
-    [1-2][0-9]) ICON=""
+    [1-2][0-9]) 
+      ICON=""
     ;;
-    *) ICON="?"
+    *) 
+      ICON="?"
+    ;;
   esac
 fi
 
