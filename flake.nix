b7cfd1f7fcc5129dede6d71098fa2c648a7aa52c
David Sanson <dsanson@gmail.com>
2026-07-17 12:46:10 -0500

    adding linux builder

--- flake.nix
+++ flake.nix
@@ -38,6 +38,22 @@
         trusted-users = ["@admin"];
       };
 
+      # builder for building rpi4 images 
+      nix.linux-builder = {
+        enable = true;
+        ephemeral = true;
+        maxJobs = 4;
+        config = {
+          virtualisation = {
+            darwin-builder = {
+              diskSize = 40 * 1024;
+              memorySize = 8 * 1024;
+            };
+            cores = 6;
+          };
+        };
+      };
+
       ids.gids.nixbld = 350;
       
       nixpkgs.config.allowUnfree = true;
